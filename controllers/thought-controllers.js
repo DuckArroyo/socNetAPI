1297fdedda6750b7f56465f48fb7b3df70914ca8
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-02 23:26:12 -0800

    added creation of thoughts and reactions

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -2,22 +2,57 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
 
-    addThought()
-    .then(dbAddThought => res.json(dbAddThought))
-    .catch((err) => res.json(err))
+    addThought({ params }, res) {
+        console.log(body);
+        
+        Thought.create(body)
 
-    addReaction()
-    .then(dbAddReaction => res.json(dbAddReaction))
-    .catch((err) => res.json(err))
- 
+        .then(({ _id }) => {
+            console.log(_id);
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thought: id } },
+                { new: true, runValidators: true }
+            );
+        })
 
-    removeThought()
+    .then(dbAddThought => {
+        if(!dbAddThought) {
+            res.status(404).json({ message: "Did not find user ID"});
+        return;
+        }
+
+        res.json(dbAddThought)
+    })
+    .catch((err) => res.json(err))
+    },
+
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { reaction: body } },
+            { new: true, runValidators: true }
+        )
+    .then(dbAddReaction => {
+        if(!dbAddReaction) {
+            res.status(404).json({ message: "Did not find user ID"});
+        return;
+        }
+
+        res.json(dbAddReaction)
+    })
+    .catch((err) => res.json(err))
+    },
+
+    removeThought({ params }, res) {
     .then(dbRemoveThought => res.json(dbRemoveThought))
     .catch((err) => res.json(err))
+    },
 
-    removeReaction()
+    removeReaction({ params }, res) {
     .then(dbRemoveReaction => res.json(dbRemoveReaction))
     .catch((err) => res.json(err))
+    },
 };
 
 module.exports = thoughtController;
