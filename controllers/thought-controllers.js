d1ffde06ea58dddabbdd2ac0b042dbbb6df9a295
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-02 23:06:27 -0800

    thought controller outline

---
+++ controllers/thought-controllers.js
@@ -0,0 +1,15 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+    addThought()
+
+    addReaction()
+
+    removeThought()
+
+    removeReaction()
+    
+};
+
+module.exports = thoughtController;
